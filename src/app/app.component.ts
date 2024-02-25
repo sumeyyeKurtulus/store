91f84eb11a29237b5dde120d7f3ffe6ed48103bc
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2024-02-25 14:16:44 +0300

    AppComponent configs ✨

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from "@angular/common";
+
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatListModule } from "@angular/material/list";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [HeaderComponent, HomeComponent, RouterOutlet, CommonModule],
+  template: ` <app-header></app-header><router-outlet></router-outlet>`,
   styles: [],
 })
 export class AppComponent {
-  title = 'store';
+  title = "store";
 }
